2d81e516f36b1b43bce1dffe121d1ea92030043b
Shiming <codingmylife@gmail.com>
2018-11-23 18:51:50 +0800

    Update initzsh.sh

--- initzsh.sh
+++ initzsh.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 ln -sf ~/.dotfile/.zshrc ~/
-ln -sf ~/.dotfile/laily.zsh-theme ~/.oh-my-zsh/themes/
+ln -sf ~/.dotfile/shiming.zsh-theme ~/.oh-my-zsh/themes/
+ln -sf ~/.dotfile/.tmux.conf ~/
